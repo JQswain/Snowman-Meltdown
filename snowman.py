d0404e90b4eb330517bc146d2e27876494aebad1
JQSwain <j.q.swain@hotmail.com>
2025-06-12 16:38:32 +0200

    Third Commit, added a display feature to the game showing the snowman and counting the mistakes

--- snowman.py
+++ snowman.py
@@ -1,5 +1,35 @@
 import random
 
+# Snowman ASCII Art stages
+STAGES = [
+    # Stage 0: Full snowman
+    """
+     ___  
+    /___\\ 
+    (o o) 
+    ( : ) 
+    ( : ) 
+    """,
+    # Stage 1: Bottom part starts melting
+    """
+     ___  
+    /___\\ 
+    (o o) 
+    ( : ) 
+    """,
+    # Stage 2: Only the head remains
+    """
+     ___  
+    /___\\ 
+    (o o) 
+    """,
+    # Stage 3: Snowman completely melted
+    """
+     ___  
+    /___\\ 
+    """
+]
+
 # List of secret words
 WORDS = ["python", "git", "github", "snowman", "meltdown"]
 
@@ -7,15 +37,35 @@ def get_random_word():
     """Selects a random word from the list."""
     return WORDS[random.randint(0, len(WORDS) - 1)]
 
+
+def display_game_state(mistakes, secret_word, guessed_letters):
+    print(STAGES[mistakes])
+    print("Word: ", end=' ')
+
+    for letter in secret_word:
+        if letter in guessed_letters:
+            print(letter, end=' ')
+        else:
+            print(" _ ", end='')
+    print()
+
+
 def play_game():
     secret_word = get_random_word()
     print("Welcome to Snowman Meltdown!")
     print("Secret word selected: " + secret_word)  # for testing, later remove this line
 
     # TODO: Build your game loop here.
-    # For now, simply prompt the user once:
-    guess = input("Guess a letter: ").lower()
-    print("You guessed:", guess)
+    mistake_counter = 0
+    guessed_letters = []
+    while mistake_counter < 3:
+        display_game_state(mistake_counter, secret_word, guessed_letters)
+        guess = input("Guess a letter: ").lower()
+        print("You guessed:", guess)
+        if guess not in secret_word:
+            mistake_counter += 1
+        guessed_letters.append(guess)
+
 
 if __name__ == "__main__":
     play_game()
